e9acaa78e94cc7043fcaa91925d0244674f13cea
Sophia Crawshaw <sophiavcrawshaw@gmail.com>
2025-12-07 23:37:13 -0500

    Fix audio and image paths for GitHub Pages

--- js/player-data.js
+++ js/player-data.js
@@ -2,14 +2,14 @@ window.PLAYLIST = [
     {
       title: "All Flowers In Time Bend Towards The Sun",
       artist: "Jeff Buckley & Elizabeth Fraser",
-      src: "/assets/audio/all-flowers-in-time.mp3",
-      cover: "/assets/img/all-flowers-in-time.jpg" // optional—swap to a real image or delete
+      src: "./assets/audio/all-flowers-in-time.mp3",
+      cover: "./assets/img/all-flowers-in-time.jpg" // optional—swap to a real image or delete
     },
     {
         title: "Fake Plastic Trees",
         artist: "Radiohead",
-        src: "/assets/audio/fake-plastic-trees.mp3",
-        cover: "/assets/img/fake-plastic-trees.png" // optional—swap to a real image or delete
+        src: "./assets/audio/fake-plastic-trees.mp3",
+        cover: "./assets/img/fake-plastic-trees.png" // optional—swap to a real image or delete
     }
   ];
   
