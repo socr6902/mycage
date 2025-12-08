09a34f7aa79bc5e4e84235baae74a3fb47276663
Sophia Crawshaw <sophiavcrawshaw@gmail.com>
2025-12-08 11:02:31 -0500

    Change to two-column layout: left column has music player and notepad, right column has video player

--- js/player-data.js
+++ js/player-data.js
@@ -3,13 +3,43 @@ window.PLAYLIST = [
       title: "All Flowers In Time Bend Towards The Sun",
       artist: "Jeff Buckley & Elizabeth Fraser",
       src: "./assets/audio/all-flowers-in-time.mp3",
-      cover: "./assets/img/all-flowers-in-time.jpg" // optional—swap to a real image or delete
+      cover: "./assets/img/all-flowers-in-time.jpg" 
     },
     {
         title: "Fake Plastic Trees",
         artist: "Radiohead",
         src: "./assets/audio/fake-plastic-trees.mp3",
-        cover: "./assets/img/fake-plastic-trees.png" // optional—swap to a real image or delete
+        cover: "./assets/img/fake-plastic-trees.png" 
+    },
+    {
+        title: "Charlie on the MTA",
+        artist: "Kingston Trio",
+        src: "./assets/audio/MTA.mp3",
+        cover: "./assets/img/MTA.jpg" 
+    },
+    {
+        title: "Chinatown — Live at Electric Lady",
+        artist: "Bleachers ft. Bruce Springsteen",
+        src: "./assets/audio/chinatown.mp3",
+        cover: "./assets/img/chinatown.jpg" 
+    },
+    {
+        title: "Merry Christmas, Please Dont Call",
+        artist: "Bleachers",
+        src: "./assets/audio/merry-christmas.mp3",
+        cover: "./assets/img/merry-christmas.jpeg" 
+    },
+    {
+        title: "Moon River",
+        artist: "Frank Sinatra",
+        src: "./assets/audio/moon-river.mp3",
+        cover: "./assets/img/moon-river.jpg" 
+    },
+    {
+        title: "Nutshell",
+        artist: "Alice in Chains",
+        src: "./assets/audio/nutshell.mp3",
+        cover: "./assets/img/nutshell.jpg" 
     }
   ];
   
