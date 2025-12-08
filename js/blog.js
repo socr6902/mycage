d4c6c49cc62b0139769a2f3ea6c2b188d00ce114
Sophia Crawshaw <sophiavcrawshaw@gmail.com>
2025-12-08 12:17:55 -0500

    captions

--- js/blog.js
+++ js/blog.js
@@ -5,9 +5,33 @@ const MEDIA_NOTES = {
     artist: "Jeff Buckley & Elizabeth Fraser",
     note: "This is love i think. This song is unreleased but was leaked after Jeffs' death against the wishes of Elizabeth. It reminds me of all the music that i'd love but will never hear. If heaven is real, there's a way for me to hear them all."
   },
+  "Charlie on the MTA": {
+    artist: "Kingston Trio",
+    note: "Absolute pinnacle of nostalgia. It reminds me of the songs you hear in your head when you take too much Benadryl."
+  },
+  "Chinatown — Live at Electric Lady": {
+    artist: "Bleachers ft. Bruce Springsteen",
+    note: "This sounds like windchimes. I almost got 'chase every feeling' tatooed on me because of this song. Looking back, I probably should have."
+  },
   "Fake Plastic Trees": {
     artist: "Radiohead",
-    note: "The build-up in this song always gets me. Such raw emotion. Perfect for rainy days."
+    note: "IF I COULD BE WHO YOU WANTED, ALL THE TIME. "
+  },
+  "Gagging Order": {
+    artist: "Radiohead",
+    note: "Different from Radiohead's usual sound, i love it."
+  },
+  "Merry Christmas, Please Dont Call": {
+    artist: "Bleachers",
+    note: "I've heard it said that there is intimacy in never speaking again. I disagree. Merry Christmas, please dont call.   "
+  },
+  "Moon River": {
+    artist: "Frank Sinatra",
+    note: "You know the scene in SATC where Big and Carrie dance to this in his empty apartment? Guy was an absolute villian. "
+  },
+  "Nutshell": {
+    artist: "Alice In Chains",
+    note: "This song first introduced me to the subgenre of 'songs you would want to bleed out to'. I can see it so clearly. 10/10. "
   },
 
   // Video notes (use the video title as the key)
